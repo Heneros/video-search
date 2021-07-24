c287c1e3227a6a4640ec01bb2cc53e556728ac06
Heneros <punk60826@gmail.com>
2021-07-24 03:25:29 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,11 @@ import VideoDetail  from './VideoDetail';
 
 class App extends React.Component{
     
+    componentDidMount(){
+      this.onTermSubmit('usa')
+    }
+
+
    state = {videos: [], selectedVideo: null};
 
     onTermSubmit = async (term) =>{
@@ -15,7 +20,10 @@ class App extends React.Component{
           }
       });
 
-    this.setState({videos: response.data.items});
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
     };
 
     onVideoSelect = video => {
@@ -26,8 +34,16 @@ class App extends React.Component{
         return ( 
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
+            <div className="ui grid">
+            <div className="ui row">
+            <div className="eleven wide column">
             <VideoDetail video={this.state.selectedVideo}/>
+            </div>
+            <div className="five wide column">
             <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+            </div>
+            </div>
            </div>)
   
         
